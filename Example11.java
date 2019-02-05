f5fb4209413713df93512864f64fbe7be4fb20a9
Anmol Ahuja <anmolahuja62@gmail.com>
2019-02-05 22:19:20 +0530

    constructors

---
+++ Example11.java
@@ -0,0 +1,34 @@
+
+public class Example11 {
+     String s="";
+	public static void main(String[] args)
+	{ System.out.println("hello world "); 
+	  yu oi = new yu ();
+	  oi.yut ();
+	 }
+
+public Example11 (String x)
+{ 
+	s=x;
+}
+public void disp ( )
+{ 
+	System.out.println(s);	
+}
+public Example11 ()
+{
+	s="no jaadu";
+}
+}
+ class yu {
+	 void yut ()
+	 { Example11[] ob = new Example11 [5];
+        for (int i=0;i<5;i++)
+        {  if (i%2==0)
+        	{ob[i] = new Example11("jaadu");}
+        else 
+           ob[i] = new Example11 ();
+            ob[i].disp (); 
+ 
+        }}}
+
